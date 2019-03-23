34bc79b796046c9d16ee6194acc6d3d77c5f92bf
veljko sirovljevic <sirovljevic@hotmail.com>
2019-03-23 13:23:39 +0100

    Added htaccess and config file

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+// Define DB Params
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "school");
+
+// Define URL
+define("ROOT_PATH", "/quantox.develop/");
+define("ROOT_URL", "http://quantox.develop/");
